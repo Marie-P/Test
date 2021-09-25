c5c40146387d4dfc2408e7c3cb820210555a5eec
Marie-P <47229106+Marie-P@users.noreply.github.com>
2021-09-25 15:39:12 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ class Main {
   constructor() {
     this.controller = new Controller();
 
-    document.documentElement.style.overflow = 'hidden';
+    // document.documentElement.style.overflow = 'hidden';
 
     window.addEventListener("keydown", event => this.controller.keydownFunction(event));
     window.addEventListener("keyup", event => this.controller.keyupFunction(event));
@@ -14,11 +14,11 @@ class Main {
     window.addEventListener("mousemove", event => this.controller.mousemoveFunction(event));
     window.addEventListener("mouseup", event => this.controller.mouseupFunction(event));
 
-      window.addEventListener("mousedown", event => this.controller.mousedownFunction(event));
+    window.addEventListener("mousedown", event => this.controller.mousedownFunction(event));
 
-      window.addEventListener("touchstart", event => this.controller.touchstartFunction(event));
-      window.addEventListener("touchmove", event => this.controller.touchmoveFunction(event));
-      window.addEventListener("touchend", event => this.controller.touchendFunction(event));
+    window.addEventListener("touchstart", event => this.controller.touchstartFunction(event));
+    window.addEventListener("touchmove", event => this.controller.touchmoveFunction(event));
+    window.addEventListener("touchend", event => this.controller.touchendFunction(event));
 
     this.controller.mainMenu();
     // this.controller.load(); 
